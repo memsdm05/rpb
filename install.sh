a604d42de228390cdb668bf872e21b930b2bee45
memsdm05 <bnbrowner@gmail.com>
2024-07-26 07:48:56 +0000

    oops2

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ go mod download
 go install -v
 sudo cp rpb.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo ln -s $GOPATH/bin/rpb /usr/local/bin/rpb
-sudo mkdir /etc/rpb
-sudo cp .env /etc/rpb
+sudo ln -s $GOPATH/bin/rpb /usr/local/bin/rpb || true
+sudo mkdir /etc/rpb || true
+sudo cp .env /etc/rpb || true
